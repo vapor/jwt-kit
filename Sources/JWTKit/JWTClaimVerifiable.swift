38098f7d869be9e9ff87aa36b0bfd384826fdaad
Scott Grosch <Scott.Grosch@icloud.com>
2020-02-03 07:57:11 -0800

    Changed the extension to be public

--- Sources/JWTKit/JWTClaimVerifiable.swift
+++ Sources/JWTKit/JWTClaimVerifiable.swift
@@ -6,20 +6,20 @@ public protocol JWTClaimVerifiable: JWTClaim, Equatable, ExpressibleByStringLite
     func verify(is desired: Value) throws
 }
 
-extension JWTClaimVerifiable {
-    public func verify(is desired: Value) throws {
+public extension JWTClaimVerifiable {
+    func verify(is desired: Value) throws {
         guard desired == self.value else {
             throw JWTError.claimVerificationFailure(name: Self.claimName, reason: "Issuer is incorrect.")
         }
     }
 
-    public func verify(oneOf desired: Value...) throws {
+    func verify(oneOf desired: Value...) throws {
         guard desired.contains(self.value) else {
             throw JWTError.claimVerificationFailure(name: Self.claimName, reason: "Not one of the allowed values.")
         }
     }
 
-    public func verify(oneOf desired: [Value]) throws {
+    func verify(oneOf desired: [Value]) throws {
         guard desired.contains(self.value) else {
             throw JWTError.claimVerificationFailure(name: Self.claimName, reason: "Not one of the allowed values.")
         }
