88e4efaa4d3d614c67c3b50be57f8290b0130453
Tim <0xtimc@gmail.com>
2020-02-27 08:02:54 +0000

    Styling on generics

--- Sources/JWTKit/Signing/HMAC/HMACSigner.swift
+++ Sources/JWTKit/Signing/HMAC/HMACSigner.swift
@@ -1,14 +1,14 @@
 import Crypto
 import Foundation
 
-internal struct HMACSigner<SHA_TYPE>: JWTAlgorithm where SHA_TYPE: HashFunction {
+internal struct HMACSigner<SHAType>: JWTAlgorithm where SHAType: HashFunction {
     let key: SymmetricKey
     let name: String
 
     func sign<Plaintext>(_ plaintext: Plaintext) throws -> [UInt8]
         where Plaintext: DataProtocol
     {
-        let authentication = Crypto.HMAC<SHA_TYPE>.authenticationCode(for: plaintext, using: self.key)
+        let authentication = Crypto.HMAC<SHAType>.authenticationCode(for: plaintext, using: self.key)
         return Array(authentication)
     }
 }
