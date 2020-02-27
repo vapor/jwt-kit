91e01d8a2b6bed648aaa52a7d63aec8ff89a5e9a
Tim <0xtimc@gmail.com>
2020-02-27 08:10:17 +0000

    Fix spacing error

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         MANGLE_END */
     ],
     dependencies: [
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "CJWTKitBoringSSL"),
