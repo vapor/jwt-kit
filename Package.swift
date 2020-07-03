f8f32f85908c890260b5134eddf7b98ba728579f
Tim <0xtimc@gmail.com>
2020-07-03 10:49:56 +0100

    Enable iOS target so it builds for ARM64

--- Package.swift
+++ Package.swift
@@ -4,13 +4,14 @@ import PackageDescription
 let package = Package(
     name: "jwt-kit",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
     ],
     products: [
         .library(name: "JWTKit", targets: ["JWTKit"]),
-        /* This target is used only for symbol mangling. It's added and removed automatically because it emits build warnings. MANGLE_START*/
+        /* This target is used only for symbol mangling. It's added and removed automatically because it emits build warnings. MANGLE_START
         .library(name: "CJWTKitBoringSSL", type: .static, targets: ["CJWTKitBoringSSL"]),
-        /*MANGLE_END */
+        MANGLE_END */
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
