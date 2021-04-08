d2f51e8e3c43228505a3b5baab359495e110b3fc
Tim <0xtimc@gmail.com>
2021-04-08 17:16:39 +0100

    Get it compiling

--- Sources/JWTKit/RSA/RSAKey.swift
+++ Sources/JWTKit/RSA/RSAKey.swift
@@ -124,24 +124,8 @@ public final class RSAKey: OpenSSLKey {
         exponent: String,
         privateExponent: String? = nil
     ) {
-        func decode(_ string: String) -> [UInt8] {
-            return [UInt8](string.utf8).base64URLDecodedBytes()
-        }
-        let n = decode(modulus)
-        let e = decode(exponent)
-        let d = privateExponent.flatMap { decode($0) }
-
-        guard let rsa = CJWTKitBoringSSL_RSA_new() else {
-            return nil
-        }
-
-        CJWTKitBoringSSL_RSA_set0_key(
-            rsa,
-            CJWTKitBoringSSL_BN_bin2bn(n, numericCast(n.count), nil),
-            CJWTKitBoringSSL_BN_bin2bn(e, numericCast(e.count), nil),
-            d.flatMap { CJWTKitBoringSSL_BN_bin2bn($0, numericCast($0.count), nil) }
-        )
-        self.init(rsa, d == nil ? .public : .private)
+        #warning("Finish")
+        return nil
     }
 
     private init(privateKey: _RSA.Signing.PrivateKey?, publicKey: _RSA.Signing.PublicKey?) throws {
