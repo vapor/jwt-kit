e893c9aba1bdbf9560f85da9647f6bd85c98d888
Tim <0xtimc@gmail.com>
2021-04-08 17:03:50 +0100

    Create new versions of RSAKey and RSASigner

--- Sources/JWTKit/RSA/RSAKey.swift
+++ Sources/JWTKit/RSA/RSAKey.swift
@@ -1,5 +1,133 @@
 @_implementationOnly import CJWTKitBoringSSL
 import struct Foundation.Data
+import _CryptoExtras
+
+public final class RSAKey2: OpenSSLKey {
+    let publicKeyBacking: _RSA.Signing.PublicKey?
+    let privateKeyBacking: _RSA.Signing.PrivateKey?
+
+    /// Creates RSAKey from public key pem file.
+    ///
+    /// Public key pem files look like:
+    ///
+    ///     -----BEGIN PUBLIC KEY-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END PUBLIC KEY-----
+    ///
+    /// This key can only be used to verify JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func `public`(pem string: String) throws -> RSAKey2 {
+        try RSAKey2(privateKey: nil, publicKey: .init(pemRepresentation: string))
+    }
+
+    /// Creates RSAKey from public key pem file.
+    ///
+    /// Public key pem files look like:
+    ///
+    ///     -----BEGIN PUBLIC KEY-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END PUBLIC KEY-----
+    ///
+    /// This key can only be used to verify JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func `public`<Data>(pem data: Data) throws -> RSAKey2
+        where Data: DataProtocol
+    {
+        fatalError()
+    }
+
+    /// Creates RSAKey from public certificate pem file.
+    ///
+    /// Certificate pem files look like:
+    ///
+    ///     -----BEGIN CERTIFICATE-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END CERTIFICATE-----
+    ///
+    /// This key can only be used to verify JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func certificate(pem string: String) throws -> RSAKey2 {
+        fatalError()
+    }
+
+    /// Creates RSAKey from public certificate pem file.
+    ///
+    /// Certificate pem files look like:
+    ///
+    ///     -----BEGIN CERTIFICATE-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END CERTIFICATE-----
+    ///
+    /// This key can only be used to verify JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func certificate<Data>(pem data: Data) throws -> RSAKey2
+        where Data: DataProtocol
+    {
+        fatalError()
+    }
+
+    /// Creates RSAKey from private key pem file.
+    ///
+    /// Private key pem files look like:
+    ///
+    ///     -----BEGIN PRIVATE KEY-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END PRIVATE KEY-----
+    ///
+    /// This key can be used to verify and sign JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func `private`(pem string: String) throws -> RSAKey2 {
+        try RSAKey2(privateKey: .init(pemRepresentation: string), publicKey: nil)
+    }
+
+    /// Creates RSAKey from private key pem file.
+    ///
+    /// Private key pem files look like:
+    ///
+    ///     -----BEGIN PRIVATE KEY-----
+    ///     MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0cOtPjzABybjzm3fCg1aCYwnx
+    ///     ...
+    ///     aX4rbSL49Z3dAQn8vQIDAQAB
+    ///     -----END PRIVATE KEY-----
+    ///
+    /// This key can be used to verify and sign JWTs.
+    ///
+    /// - parameters:
+    ///     - pem: Contents of pem file.
+    public static func `private`<Data>(pem data: Data) throws -> RSAKey2
+        where Data: DataProtocol
+    {
+        fatalError()
+    }
+
+    private init(privateKey: _RSA.Signing.PrivateKey?, publicKey: _RSA.Signing.PublicKey?) throws {
+        guard privateKey != nil || publicKey != nil else {
+            throw RSAError.keyInitializationFailure
+        }
+        self.privateKeyBacking = privateKey
+        self.publicKeyBacking = publicKey
+    }
+}
 
 public final class RSAKey: OpenSSLKey {
     /// Creates RSAKey from public key pem file.
--- Sources/JWTKit/RSA/RSASigner.swift
+++ Sources/JWTKit/RSA/RSASigner.swift
@@ -1,5 +1,34 @@
 @_implementationOnly import CJWTKitBoringSSL
 import struct Foundation.Data
+import _CryptoExtras
+
+internal struct RSASigner2: JWTAlgorithm {
+
+    let key: RSAKey2
+    let name: String
+
+    func sign<Plaintext>(_ plaintext: Plaintext) throws -> [UInt8] where Plaintext : DataProtocol {
+        guard let privateKey = key.privateKeyBacking else {
+            throw RSAError.privateKeyRequired
+        }
+        let bytes = try privateKey.signature(for: plaintext).rawRepresentation
+        return Array(bytes)
+    }
+
+    func verify<Signature, Plaintext>(_ signature: Signature, signs plaintext: Plaintext) throws -> Bool where Signature : DataProtocol, Plaintext : DataProtocol {
+        let publicKey: _RSA.Signing.PublicKey
+        if let key = self.key.publicKeyBacking {
+            publicKey = key
+        } else if let key = self.key.privateKeyBacking?.publicKey {
+            publicKey = key
+        } else {
+            // Something bad has gone wrong as we should be able to get into this state
+            throw RSAError.keyInitializationFailure
+        }
+        let rsaSignature = _RSA.Signing.RSASignature(rawRepresentation: signature)
+        return try publicKey.isValidSignature(rsaSignature, for: plaintext)
+    }
+}
 
 internal struct RSASigner: JWTAlgorithm, OpenSSLSigner {
     let key: RSAKey
