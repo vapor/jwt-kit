aa7d9cd805b10c1ca17dfc53062ffbc695469cf4
Babu Frik <103904145+thebabufrik@users.noreply.github.com>
2022-07-08 19:49:58 -0400

    watchOS support (#76)

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(name: "JWTKit", targets: ["JWTKit"]),
