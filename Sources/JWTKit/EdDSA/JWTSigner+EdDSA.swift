88d935acd20878cd3390d600671e185788b0737a
Martin Gratzer <martin@topmind.eu>
2022-08-17 15:44:47 +0200

    Define JWTSigner extension keys as ContiguousBytes

--- Sources/JWTKit/EdDSA/JWTSigner+EdDSA.swift
+++ Sources/JWTKit/EdDSA/JWTSigner+EdDSA.swift
@@ -2,7 +2,7 @@ import Foundation
 import Crypto
 
 extension JWTSigner {
-	public static func eddsa(publicKey: Data, privateKey: Data) -> JWTSigner {
+	public static func eddsa<D: ContiguousBytes>(publicKey: D, privateKey: D) -> JWTSigner {
 		.init(
 			algorithm: EdDSASigner(
 				publicKey: publicKey,
