3b6e5a531cb2043c14b5c73237eff3b9814e8e4b
Matteo Franceschi <matteo.franceschi@flowpay.it>
2022-11-22 12:54:32 +0100

    add implementation of pss type algorithm

--- Sources/JWTKit/RSA/JWTSigner+RSA.swift
+++ Sources/JWTKit/RSA/JWTSigner+RSA.swift
@@ -5,7 +5,8 @@ extension JWTSigner {
         return .init(algorithm: RSASigner(
             key: key,
             algorithm: CJWTKitBoringSSL_EVP_sha256(),
-            name: "RS256"
+            name: "RS256",
+            usePSS: false
         ))
     }
 
@@ -13,7 +14,8 @@ extension JWTSigner {
         return .init(algorithm: RSASigner(
             key: key,
             algorithm: CJWTKitBoringSSL_EVP_sha384(),
-            name: "RS384"
+            name: "RS384",
+            usePSS: false
         ))
     }
 
@@ -21,7 +23,35 @@ extension JWTSigner {
         return .init(algorithm: RSASigner(
             key: key,
             algorithm: CJWTKitBoringSSL_EVP_sha512(),
-            name: "RS512"
+            name: "RS512",
+            usePSS: false
+        ))
+    }
+
+    public static func ps256(key: RSAKey) -> JWTSigner {
+        return .init(algorithm: RSASigner(
+            key: key,
+            algorithm: CJWTKitBoringSSL_EVP_sha256(),
+            name: "PS256",
+            usePSS: true
+        ))
+    }
+
+    public static func ps384(key: RSAKey) -> JWTSigner {
+        return .init(algorithm: RSASigner(
+            key: key,
+            algorithm: CJWTKitBoringSSL_EVP_sha384(),
+            name: "PS384",
+            usePSS: true
+        ))
+    }
+
+    public static func ps512(key: RSAKey) -> JWTSigner {
+        return .init(algorithm: RSASigner(
+            key: key,
+            algorithm: CJWTKitBoringSSL_EVP_sha512(),
+            name: "PS512",
+            usePSS: true
         ))
     }
 }
--- Sources/JWTKit/RSA/RSASigner.swift
+++ Sources/JWTKit/RSA/RSASigner.swift
@@ -5,6 +5,7 @@ internal struct RSASigner: JWTAlgorithm, OpenSSLSigner {
     let key: RSAKey
     let algorithm: OpaquePointer
     let name: String
+    let usePSS: Bool
 
     func sign<Plaintext>(_ plaintext: Plaintext) throws -> [UInt8]
         where Plaintext: DataProtocol
@@ -12,25 +13,45 @@ internal struct RSASigner: JWTAlgorithm, OpenSSLSigner {
         guard case .private = self.key.type else {
             throw JWTError.signingAlgorithmFailure(RSAError.privateKeyRequired)
         }
-        var signatureLength: UInt32 = 0
+
         var signature = [UInt8](
             repeating: 0,
             count: Int(CJWTKitBoringSSL_RSA_size(key.c))
         )
 
         let digest = try self.digest(plaintext)
-        guard CJWTKitBoringSSL_RSA_sign(
-            CJWTKitBoringSSL_EVP_MD_type(self.algorithm),
-            digest,
-            numericCast(digest.count),
-            &signature,
-            &signatureLength,
-            self.key.c
-        ) == 1 else {
+        let signingResult: Int32
+        let signLen: Int
+        if self.usePSS {
+            var signatureLength: Int = 0
+            signingResult = CJWTKitBoringSSL_RSA_sign_pss_mgf1(
+                self.key.c,
+                &signatureLength,
+                &signature,
+                signature.count,
+                digest,
+                numericCast(digest.count),
+                self.algorithm,
+                nil,
+                -1)
+            signLen = signatureLength
+        } else {
+            var signatureLength: UInt32 = 0
+            signingResult = CJWTKitBoringSSL_RSA_sign(
+                CJWTKitBoringSSL_EVP_MD_type(self.algorithm),
+                digest,
+                numericCast(digest.count),
+                &signature,
+                &signatureLength,
+                self.key.c
+            )
+            signLen = numericCast(signatureLength)
+        }
+        guard signingResult == 1 else {
             throw JWTError.signingAlgorithmFailure(RSAError.signFailure)
         }
 
-        return .init(signature[0..<numericCast(signatureLength)])
+        return .init(signature[0..<numericCast(signLen)])
     }
 
     func verify<Signature, Plaintext>(
@@ -41,13 +62,25 @@ internal struct RSASigner: JWTAlgorithm, OpenSSLSigner {
     {
         let digest = try self.digest(plaintext)
         let signature = signature.copyBytes()
-        return CJWTKitBoringSSL_RSA_verify(
-            CJWTKitBoringSSL_EVP_MD_type(self.algorithm),
-            digest,
-            numericCast(digest.count),
-            signature,
-            numericCast(signature.count),
-            self.key.c
-        ) == 1
+        if self.usePSS {
+            return CJWTKitBoringSSL_RSA_verify_pss_mgf1(
+                self.key.c,
+                digest,
+                digest.count,
+                self.algorithm,
+                nil,
+                -1,
+                signature,
+                signature.count) == 1
+        } else {
+            return CJWTKitBoringSSL_RSA_verify(
+                CJWTKitBoringSSL_EVP_MD_type(self.algorithm),
+                digest,
+                numericCast(digest.count),
+                signature,
+                numericCast(signature.count),
+                self.key.c
+            ) == 1
+        }
     }
 }
