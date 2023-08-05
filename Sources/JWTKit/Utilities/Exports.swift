14fdfc6182d8fe7db950714b4b8a600d4f73f573
Tim <0xtimc@gmail.com>
2023-08-05 16:38:17 +0100

    Update exports

--- Sources/JWTKit/Utilities/Exports.swift
+++ Sources/JWTKit/Utilities/Exports.swift
@@ -1,4 +1,10 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import struct Foundation.Date
+@_documentation(visibility: internal) @_exported import protocol Foundation.DataProtocol
+@_documentation(visibility: internal) @_exported import protocol Foundation.ContiguousBytes
+
+#else
 
 @_exported import struct Foundation.Date
 @_exported import protocol Foundation.DataProtocol
