6ece739093eaa98090049b93f28a7ba94d84e8df
Paul <paultoffoloni@gmail.com>
2023-11-09 12:37:23 +0100

    Rename signer to algorithm

--- Sources/JWTKit/JWTPayload.swift
+++ Sources/JWTKit/JWTPayload.swift
@@ -2,5 +2,5 @@
 /// Each variable represents a claim.
 public protocol JWTPayload: Codable, Sendable {
     /// Verifies that the payload's claims are correct or throws an error.
-    func verify(using signer: JWTAlgorithm) async throws
+    func verify(using algorithm: JWTAlgorithm) async throws
 }
