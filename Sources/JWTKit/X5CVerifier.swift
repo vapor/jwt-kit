d3f08e6b1b459a39ca10d35b7d9a96022e0b8ff9
Paul <paultoffoloni@gmail.com>
2023-11-09 15:40:52 +0100

    Add support for custom time validation X5Cs

--- Sources/JWTKit/X5CVerifier.swift
+++ Sources/JWTKit/X5CVerifier.swift
@@ -121,13 +121,27 @@ public struct X5CVerifier: Sendable {
             return data
         }
 
-        // Setup an untrusted chain using all the certificates in the x5c.
+        // Setup an untrusted chain using all the certificates in the x5c
         let untrustedChain = try CertificateStore(certificateData.map {
             try Certificate(derEncoded: [UInt8]($0))
         })
 
+        var date = Date()
+        var decoder = jsonDecoder
+        if Payload.self is ValidationTimePayload.Type {
+            decoder.dateDecodingStrategy = .millisecondsSince1970
+        }
+
+        let payload = try parser.payload(as: Payload.self, jsonDecoder: decoder)
+
+        if let validationTimePayload = payload as? ValidationTimePayload {
+            date = validationTimePayload.signedDate
+        }
+
         // Setup the verifier using the predefined trusted store
-        var verifier = Verifier(rootCertificates: trustedStore) { RFC5280Policy(validationTime: Date()) }
+        var verifier = Verifier(rootCertificates: trustedStore) {
+            RFC5280Policy(validationTime: date)
+        }
 
         // Extract the leaf certificate (first certificate in x5c)
         let leafCertificate = try Certificate(derEncoded: [UInt8](certificateData[0]))
@@ -139,10 +153,14 @@ public struct X5CVerifier: Sendable {
             throw JWTError.generic(identifier: "JWS", reason: "Invalid x5c chain: \(failures)")
         }
 
-        // Assuming the chain is valid, verify the token was signed by the valid certificate.
+        // Assuming the chain is valid, verify the token was signed by the valid certificate
         let ecdsaKey = try ES256Key.certificate(pem: leafCertificate.serializeAsPEM().pemString)
 
         let signer = JWTSigner(algorithm: ECDSASigner(key: ecdsaKey, algorithm: .sha256, name: headerAlg))
         return try await signer.verify(parser: parser)
     }
+}
+
+public protocol ValidationTimePayload: JWTPayload {
+    var signedDate: Date { get }
 }
