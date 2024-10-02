b0c7d3ecb5d689d15cd1d7a3f050d2c6022db53b
Paul Toffoloni <paultoffoloni@gmail.com>
2024-10-02 23:22:50 +0200

    Fix package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "JWTKit", targets: ["JWTKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", branch: "3.8.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.8.0"),
         .package(url: "https://github.com/apple/swift-certificates.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
