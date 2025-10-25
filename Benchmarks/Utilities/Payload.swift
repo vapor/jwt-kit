1e4642c7ca1542180adb3f09efd6041afa109226
Paul Toffoloni <paultoffoloni@gmail.com>
2025-10-25 10:46:10 +0200

    Fix

--- Benchmarks/Utilities/Payload.swift
+++ Benchmarks/Utilities/Payload.swift
@@ -4,7 +4,7 @@ package struct Payload: JWTPayload {
     package let name: String
     package let admin: Bool
 
-    func verify(using signer: some JWTAlgorithm) async throws {
+    package func verify(using signer: some JWTAlgorithm) async throws {
         // nothing to verify
     }
 }
